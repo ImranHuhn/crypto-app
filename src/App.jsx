486ffc93c660e37380f7eea4bb616783f7421485
ImranRovano <imranhuhn1@gmail.com>
2023-04-14 10:26:11 -0400

    removed hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css';
 
 class App extends React.Component  {
   render() {
-    const [count, setCount] = useState(0);
     return (
       <div className="App">
         <div>
@@ -19,9 +17,6 @@ class App extends React.Component  {
         </div>
         <h1>Vite + React</h1>
         <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
           <p>
             Edit <code>src/App.jsx</code> and save to test HMR
           </p>
