5f9dc840e3967e785a0ff0b3abfc0f9600d4e1cb
ImranRovano <imranhuhn1@gmail.com>
2023-04-30 00:22:13 -0400

    changed infinite scrolling package

--- src/pages/Coins/index.jsx
+++ src/pages/Coins/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
-import InfiniteScroll from "react-infinite-scroller";
+// import InfiniteScroll from "react-infinite-scroll-component";
 import { getCoins } from "../../utils/api";
 
 class Coins extends React.Component {
@@ -27,20 +27,33 @@ class Coins extends React.Component {
   render() {
     console.log("$$$", this.state.allCoins);
     return (
-      // <InfiniteScroll
-      //   pageStart={0}
-      //   loadMore={loadFunc}
-      //   hasMore={true || false}
-      //   loader={
-      //     <div className="loader" key={0}>
-      //       Loading ...
-      //     </div>
-      //   }
-      // >
-        <div
-          className="text"
-          style={{ width: "95%", margin: "0 auto", padding: "0 10px" }}
-        >
+      <div
+        className="text"
+        style={{ width: "95%", margin: "0 auto", padding: "0 10px" }}
+      >
+        {/* <InfiniteScroll
+          dataLength={this.state.allCoins.length} //This is important field to render the next data
+          next={fetchData}
+          hasMore={true}
+          loader={<h4>Loading...</h4>}
+          endMessage={
+            <p style={{ textAlign: "center" }}>
+              <b>Yay! You have seen it all</b>
+            </p>
+          }
+          // below props only if you need pull down functionality
+          refreshFunction={this.refresh}
+          pullDownToRefresh
+          pullDownToRefreshThreshold={20}
+          pullDownToRefreshContent={
+            <h3 style={{ textAlign: "center" }}>
+              &#8595; Pull down to refresh
+            </h3>
+          }
+          releaseToRefreshContent={
+            <h3 style={{ textAlign: "center" }}>&#8593; Release to refresh</h3>
+          }
+        > */}
           <h1 className="text">Your overview</h1>
           <table
             className="color3"
@@ -120,8 +133,8 @@ class Coins extends React.Component {
               })}
             </tbody>
           </table>
-        </div>
-      // </InfiniteScroll>
+        {/* </InfiniteScroll> */}
+      </div>
     );
   }
 }
