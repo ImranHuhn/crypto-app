d8164060c33973858e79910c25780b493d47a5f3
ImranRovano <imranhuhn1@gmail.com>
2023-05-11 12:22:31 -0400

    removed console logs

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -2,7 +2,6 @@ import axios from "axios";
 
 export const getCoins = async (parameter) => {
   let order, page;
-  console.log("object", typeof parameter === "object");
 
   if (typeof parameter === "object") {
     parameter;
@@ -25,7 +24,6 @@ export const getCoins = async (parameter) => {
     const { data } = await axios(
       `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=${order}&per_page=50&page=${page}&sparkline=true&price_change_percentage=1h%2C24h%2C7d`
     );
-    console.log(data);
     return data;
   } catch (error) {
     alert(error);
