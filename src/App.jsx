412b8b68acb8177e094983f379e040a5e2c8a1fa
Imran Rovano <52342912+ImranRovano@users.noreply.github.com>
2023-05-11 10:25:56 -0400

    Revert "Landing page setup table"

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,29 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
-import { getMarketData } from "./utils/api";
 import { GlobalStyle, lightTheme, darkTheme } from "./App.styles";
-import { Navbar } from "./components/Navbar";
-import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Coins from "./pages/Coins";
 import Portfolio from "./pages/Portfolio";
 
 class App extends React.Component {
   state = {
     on: false,
-    marketData: null,
-  };
-
-  handleMarketData = async () => {
-    const newData = await getMarketData();
-    this.setState({
-      marketData: newData,
-    });
   };
 
   handleClick = () => {
-    const { on } = this.state;
-    const themeSetting = !on;
-    this.setState({ on: themeSetting });
-    localStorage.setItem("themeSetting", JSON.stringify(themeSetting));
-  };
-
-  componentDidMount = () => {
-    this.handleMarketData();
-    const storedTheme = JSON.parse(localStorage.getItem("themeSetting"));
-    this.setState({ on: storedTheme });
+    this.setState({ on: !this.state.on });
   };
 
   render() {
-    const { on } = this.state;
-    const { active_cryptocurrencies, markets, total_market_cap, total_volume } =
-      this.state.marketData || {};
     return (
-      <ThemeProvider theme={on ? lightTheme : darkTheme}>
+      <ThemeProvider theme={this.state.on ? lightTheme : darkTheme}>
         <GlobalStyle />
         <Router>
           <div>
-            <Navbar
-              handleClick={this.handleClick}
-              on={on}
-              totalCoins={active_cryptocurrencies}
-              totalExchanges={markets}
-              marketCap={total_market_cap}
-              marketVolume={total_volume}
-            />
+            <Navbar handleClick={this.handleClick} on={this.state.on} />
             <Switch>
-              <Route
-                exact
-                path="/"
-                component={(props) => (
-                  <Home
-                    {...props}
-                    totalCoins={active_cryptocurrencies} // need this for handleInfiniteScroll function in coins page
-                  />
-                )}
-              />
+              <Route exact path="/" component={Coins} />
               <Route exact path="/portfolio" component={Portfolio} />
             </Switch>
           </div>
