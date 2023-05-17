d9d0f25a47c3551d3739df77251e0270dffa6aa5
ImranRovano <imranhuhn1@gmail.com>
2023-05-17 01:16:47 -0400

    installed faker to test charts

--- src/components/TableData/index.jsx
+++ src/components/TableData/index.jsx
@@ -3,6 +3,22 @@ import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 import { TableRow, Image } from "./TableData.styles";
 
+//////////////////////////////////////////////////////
+// import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+import { faker } from '@faker-js/faker';
+//////////////////////////////////////////////////////
+
 export const TableData = (props) => {
   const {
     market_cap_rank,
@@ -18,6 +34,64 @@ export const TableData = (props) => {
     circulating_supply,
     total_supply,
   } = props.item || {};
+
+  //////////////////////////////////////////////////////
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: true,
+        text: "Chart.js Line Chart",
+      },
+    },
+  };
+
+  const labels = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+  ];
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Dataset 1",
+        data: labels.map(() =>
+          faker.datatype.number({ min: -1000, max: 1000 })
+        ),
+        borderColor: "rgb(255, 99, 132)",
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+      {
+        label: "Dataset 2",
+        data: labels.map(() =>
+          faker.datatype.number({ min: -1000, max: 1000 })
+        ),
+        borderColor: "rgb(53, 162, 235)",
+        backgroundColor: "rgba(53, 162, 235, 0.5)",
+      },
+    ],
+  };
+  //////////////////////////////////////////////////////
+
   return (
     <TableRow>
       <td>{market_cap_rank || <Skeleton />}</td>
@@ -36,7 +110,9 @@ export const TableData = (props) => {
         {" / "}
         {total_supply || <Skeleton />}
       </td>
-      <td>9</td>
+      <td>
+      <Line options={options} data={data} />
+      </td>
     </TableRow>
   );
 };
