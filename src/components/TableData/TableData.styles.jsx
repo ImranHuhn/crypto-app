5a49819a88439ea0eee9faa14ba6ca1c66c8c203
ImranRovano <imranhuhn1@gmail.com>
2023-05-23 14:27:04 -0400

    test

--- src/components/TableData/TableData.styles.jsx
+++ src/components/TableData/TableData.styles.jsx
@@ -1,10 +1,23 @@
 import styled from "styled-components";
 
-export const FillBar = styled.div`
-  width: ${(props) => props.fillWidth}%;
-  background-color: ${(props) => props.fillColor};
-`;
+export const FillBar = styled.div.attrs((props) => ({
+  style: {
+    width: `${props.fillWidth}%`,
+    backgroundColor: `${props.fillColor}`,
+  },
+}));
+
+// export const FillBar = styled.div`
+//   width: ${(props) => props.fillWidth}%;
+//   background-color: ${(props) => props.fillColor};
+// `;
 
 export const List = styled.li`
   color: ${(props) => props.textColor};
 `;
+
+// const Component = styled.div.attrs(props => ({
+//   style: {
+//     background: props.background,
+//   },
+// }))`width: 100%;`
